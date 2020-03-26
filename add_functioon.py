b149486f52b8c0539347938d046f89e88b8f49c2
“arnaben” <arnabben@gmail.com>
2020-03-26 22:59:51 +0000

    add now supports custom delimeters

--- add_functioon.py
+++ add_functioon.py
@@ -1,6 +1,15 @@
 def add(numbers):
     if numbers == "":
         return 0
+    if numbers[0] == '/' and numbers[1] == '/' and '\n' in numbers:
+        delim = ''
+        for i in range(2, len(numbers)):
+            if numbers[i] == '\n':
+                break
+            else:
+                delim += numbers[i]
+        numbers = numbers.replace(delim, ',')
+        numbers = numbers[3+len(delim):]  # removes the // delimeter and the \n from the start of the numbers
     numbers = numbers.replace('\n', ',')
     numbers = [int(n) for n in numbers.split(",") if int(n) <= 1000]
     negative = []
