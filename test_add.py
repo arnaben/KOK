77007a09ef1735673fd6d350e1f3c7ed4ff2abed
“arnaben” <arnabben@gmail.com>
2020-03-26 15:59:42 +0000

    test added for newline as delimiter

--- test_add.py
+++ test_add.py
@@ -12,6 +12,9 @@ def test_two_numbers():
 def test_unknown_amount_of_numbers():
     assert add("1,2,3,4,5") == 15
 
+def test_newline_delim():
+    assert add("1\n2,3") == 6
+
 test_empty()
 test_one_number()
 test_two_numbers()
