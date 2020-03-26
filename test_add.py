d5f0678277d521e1ee2c3699c9ddb700043912b0
“arnaben” <arnabben@gmail.com>
2020-03-26 22:06:06 +0000

    added test for numbers over 1000

--- test_add.py
+++ test_add.py
@@ -15,8 +15,12 @@ def test_unknown_amount_of_numbers():
 def test_newline_delim():
     assert add("1\n2,3") == 6
 
+def test_big_numbers():
+    assert add("1001,1000") == 1000
+
 test_empty()
 test_one_number()
 test_two_numbers()
 test_unknown_amount_of_numbers()
 test_newline_delim()
+test_big_numbers()
