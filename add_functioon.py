71c81e8fb67a166e0395192be3fe51c8b87d7530
“arnaben” <arnabben@gmail.com>
2020-03-26 22:10:00 +0000

    add no longer adds numbers larger than 1000

--- add_functioon.py
+++ add_functioon.py
@@ -2,6 +2,6 @@ def add(numbers):
     if numbers == "":
         return 0
     numbers = numbers.replace('\n', ',')
-    numbers = [int(n) for n in numbers.split(",")]
+    numbers = [int(n) for n in numbers.split(",") if int(n) <= 1000]
 
     return sum(numbers)
