0eae44a3fff9b6d1fa4873ccbf0df7acb3f37e29
“arnaben” <arnabben@gmail.com>
2020-03-26 22:47:25 +0000

    add now raises exception with negative numbers

--- add_functioon.py
+++ add_functioon.py
@@ -3,5 +3,11 @@ def add(numbers):
         return 0
     numbers = numbers.replace('\n', ',')
     numbers = [int(n) for n in numbers.split(",") if int(n) <= 1000]
+    negative = []
+    for n in numbers:
+        if n < 0:
+            negative.append(str(n))
+    if len(negative) > 0:
+        raise Exception("Negative numbers not allowed: {}".format(','.join(negative)))
 
     return sum(numbers)
