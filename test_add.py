6ddc4226090a5056c6194b7c8dbee6a3f06be6af
“arnaben” <arnabben@gmail.com>
2020-03-26 15:57:54 +0000

    test for unknown amount of numbers added. Test passed due to previous functionality

--- test_add.py
+++ test_add.py
@@ -9,6 +9,10 @@ def test_one_number():
 def test_two_numbers():
     assert add("1,2") == 3
 
+def test_unknown_amount_of_numbers():
+    assert add("1,2,3,4,5") == 15
+
 test_empty()
 test_one_number()
 test_two_numbers()
+test_unknown_amount_of_numbers()
